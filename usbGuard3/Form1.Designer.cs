a75a9c5e0f6510cb555bcd04d5a6733773f3cbaa
Samuel <tuglersamuel@gmail.com>
2019-10-24 22:33:36 +0200

    Added project files

---
+++ usbGuard3/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace usbGuard3
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		/// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Code généré par le Concepteur Windows Form
+
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.buttonClose = new System.Windows.Forms.Button();
+			this.labelCID = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::usbGuard3.Properties.Resources.warning;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(90, 89);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(107, 12);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(129, 24);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Information";
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(108, 36);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(489, 39);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "La connection du peripherique a l\'ordinateur a ete signalée et enregistrer";
+			// 
+			// buttonClose
+			// 
+			this.buttonClose.Location = new System.Drawing.Point(522, 78);
+			this.buttonClose.Name = "buttonClose";
+			this.buttonClose.Size = new System.Drawing.Size(75, 23);
+			this.buttonClose.TabIndex = 3;
+			this.buttonClose.Text = "Fermer";
+			this.buttonClose.UseVisualStyleBackColor = true;
+			this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+			// 
+			// labelCID
+			// 
+			this.labelCID.AutoSize = true;
+			this.labelCID.Location = new System.Drawing.Point(108, 88);
+			this.labelCID.Name = "labelCID";
+			this.labelCID.Size = new System.Drawing.Size(86, 13);
+			this.labelCID.TabIndex = 4;
+			this.labelCID.Text = "Failed to get CID";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(609, 113);
+			this.Controls.Add(this.labelCID);
+			this.Controls.Add(this.buttonClose);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "usbGuard";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_OnClosing);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button buttonClose;
+		private System.Windows.Forms.Label labelCID;
+	}
+}
+
---
+++ usbGuard3/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace usbGuard3
+{
+	public partial class Form1 : Form
+	{
+		const int MF_BYCOMMAND = 0;
+		const int MF_DISABLED = 2;
+		const int SC_CLOSE = 0xF060;
+
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			closePopup();
+
+			WqlEventQuery insertQuery = new WqlEventQuery("SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+			WqlEventQuery removeQuery = new WqlEventQuery("SELECT * FROM __InstanceDeletionEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'");
+			ManagementEventWatcher insertWatcher = new ManagementEventWatcher(insertQuery);
+			ManagementEventWatcher removeWatcher = new ManagementEventWatcher(removeQuery);
+			insertWatcher.EventArrived += new EventArrivedEventHandler(DeviceInsertedEvent);
+			removeWatcher.EventArrived += new EventArrivedEventHandler(DeviceRemovedEvent);
+			insertWatcher.Start();
+			removeWatcher.Start();
+
+			String CID = Environment.UserName + "-" + Environment.MachineName;
+			log(CID,"LOGIN");
+		}
+
+		private void Form1_OnClosing(object sender, EventArgs e) {
+			String CID = Environment.UserName + "-" + Environment.MachineName;
+			log(CID,"LOGOUT");
+		}
+		
+		private void DeviceInsertedEvent(object sender, EventArrivedEventArgs e) {
+			ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+			String CID = Environment.UserName + "-" + Environment.MachineName + "-" + instance.Properties["PNPDeviceID"].Value;
+			log(CID,"INSERTED");
+			openPopup(CID);
+		}
+
+		private void DeviceRemovedEvent(object sender, EventArrivedEventArgs e) {
+			ManagementBaseObject instance = (ManagementBaseObject)e.NewEvent["TargetInstance"];
+			String CID = Environment.UserName + "-" + Environment.MachineName + "-" + instance.Properties["PNPDeviceID"].Value;
+			log(CID, "REMOVED");
+			closePopup();
+		}
+
+		private void buttonClose_Click(object sender, EventArgs e) {
+			closePopup();
+		}
+
+		private void openPopup(string cid)
+		{
+			BeginInvoke(new MethodInvoker(delegate
+			{
+				this.ShowInTaskbar = false;
+				this.Visible = false;
+				this.Visible = true;
+				this.TopMost = true;
+				this.CenterToScreen();
+				labelCID.Text = cid;
+			}));
+		}
+		private void closePopup()
+		{
+			BeginInvoke(new MethodInvoker(delegate {
+				this.TopMost = false;
+				this.Visible = false;
+				notifyIcon1.Visible = true;
+			}));
+		}
+
+		private void log(string text,string cat) {
+			string log = "["+DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss")+"] ["+ cat + "] " + text;
+			Console.WriteLine(log);
+			
+			try
+			{
+				string path = @"c:\temp\usbGuardLog.txt";
+
+				if (!File.Exists(path))
+				{
+					using (StreamWriter sw = File.CreateText(path)) { sw.WriteLine("[FIRST]"); }
+				}
+
+				using (StreamWriter sw = File.AppendText(path))
+				{
+					sw.WriteLine(log);
+				}
+
+			} catch(Exception e) {
+				Console.WriteLine(e.ToString());
+			}
+		}
+
+	}
+}
